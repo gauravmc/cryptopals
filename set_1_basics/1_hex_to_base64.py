504cfd8edaf0792d74a8f170b7a6a8845f0e9372
Gaurav Chande <gmail@gauravchande.com>
2018-08-16 12:24:52 +0530

    Simplify first two implementations

--- set_1_basics/1_hex_to_base64.py
+++ set_1_basics/1_hex_to_base64.py
@@ -1,8 +1,8 @@
-import codecs, base64
+import base64
 
 # Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
 
-def hex_to_base64(string):
-    return base64.b64encode(codecs.decode(string, 'hex'))
+def hex_to_base64(encoded_str):
+    return base64.b64encode(bytes.fromhex(encoded_str))
 
 # print(hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
--- set_1_basics/2_fixed_xor.py
+++ set_1_basics/2_fixed_xor.py
@@ -1,14 +1,7 @@
-import codecs
+def string_buffer_xor(buffer1, buffer2):
+    s1 = bytes.fromhex(buffer1)
+    s2 = bytes.fromhex(buffer2)
 
-def buffer_xor(buffer1, buffer2):
-    buffer1 = codecs.decode(buffer1, 'hex')
-    buffer2 = codecs.decode(buffer2, 'hex')
+    return bytes(b1 ^ b2 for b1, b2 in zip(s1, s2)).hex()
 
-    result = bytearray()
-
-    for byte1, byte2 in zip(buffer1, buffer2):
-        result.append(byte1 ^ byte2)
-
-    return codecs.encode(result, 'hex')
-
-# print(buffer_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))
+# print(string_buffer_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))
