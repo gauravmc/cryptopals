14a11f1cb0adc68c50dccd604f3ae2a29b4a3c13
Gaurav Chande <gmail@gauravchande.com>
2018-08-16 11:15:08 +0530

    Do not use str as variable

--- set_1_basics/1_hex_to_base64.py
+++ set_1_basics/1_hex_to_base64.py
@@ -2,7 +2,7 @@ import codecs, base64
 
 # Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
 
-def hex_to_base64(str):
-    return base64.b64encode(codecs.decode(str, 'hex'))
+def hex_to_base64(string):
+    return base64.b64encode(codecs.decode(string, 'hex'))
 
 # print(hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
