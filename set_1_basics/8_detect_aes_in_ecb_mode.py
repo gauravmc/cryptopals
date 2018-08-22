a7ed29a8350d0199e023933d21f562f0d7469eeb
Gaurav Chande <gmail@gauravchande.com>
2018-08-22 11:58:32 +0530

    Make challenge 8 test better

--- set_1_basics/8_detect_aes_in_ecb_mode.py
+++ set_1_basics/8_detect_aes_in_ecb_mode.py
@@ -6,7 +6,7 @@ import utils
 #  Remember that the problem with ECB is that it is stateless and deterministic; the same 16 byte plaintext block will always produce the same 16 byte ciphertext.
 
 def detect_which_aes_is_ecb_mode(ciphertexts):
-    key_lenghts = [16, 24, 32]
+    key_lenghts = [16, 24, 32] # Possible AES key lenghts
     distances = []
 
     for key_length in key_lenghts:
@@ -20,13 +20,17 @@ import unittest
 
 class TestSet1Challenge8(unittest.TestCase):
     def test_detect_aes_ecb_ecrypted_text(self):
-        expected = 'd880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a'
-
         with open(os.path.dirname(os.path.abspath(__file__)) + '/files/challenge_8.txt') as f:
-            hexed_texts = f.readlines()
+            hexed_texts = list(map(str.strip, f.readlines()))
             ciphertexts = list(map(bytes.fromhex, hexed_texts))
+
             result = detect_which_aes_is_ecb_mode(ciphertexts).hex()
-            self.assertEqual(expected, result)
+            expected_text = 'd880619740a8a19b7840a8a31c810a3d08649af70d'
+            self.assertEqual(expected_text, result[0:42])
+
+            line_num = hexed_texts.index(result) + 1
+            expected_line_num = 133
+            self.assertEqual(expected_line_num, line_num)
 
 if __name__ == '__main__':
     unittest.main()
